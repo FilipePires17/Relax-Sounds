c1e4633620f973df19983028844c319c65fd6f5f
FilipePires17 <filipepiresec@gmail.com>
2021-01-02 10:48:03 -0400

    título e bg color

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import '../BusinessLayer/button.dart';
+import 'package:flutter/services.dart';
+import '../BusinessLayer/painel.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,14 +10,34 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
-      home: Scaffold(
-        body: Column(
-          children: [
-            Button(),
-          ],
-        ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.indigo,
+        accentColor: Colors.teal[300],
+        textTheme: TextTheme(bodyText2: TextStyle(color: Colors.purple[50])),
       ),
+      home: Scaffold(
+          body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.blue[400], Colors.indigo[900]])),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 90.0),
+          child: Container(
+            alignment: Alignment.topCenter,
+            child: Text(
+              "Relax Sounds",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      )),
     );
   }
 }
