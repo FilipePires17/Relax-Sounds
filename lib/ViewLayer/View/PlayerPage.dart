03903aca92e61e87a16a878eaf5ac7ad24831587
henrisama <pedropiresflores@icloud.com>
2021-02-03 20:12:33 -0400

    PlayerPage

--- lib/ViewLayer/View/PlayerPage.dart
+++ lib/ViewLayer/View/PlayerPage.dart
@@ -1,6 +1,10 @@
+
+
 import 'package:flutter/material.dart';
 import '../Widgets/PlayerWidget.dart';
-//import '../../BusinessLogicLayer/FunctionalRequirements/PlayerFR.dart';
+//import 'package:audioplayers/audioplayers.dart';
+//import 'package:audioplayers/audio_cache.dart';
+import '../../BusinessLogicLayer/FunctionalRequirements/PlayerFR.dart';
 
 class PlayerPage extends StatefulWidget {
   @override
@@ -8,7 +12,19 @@ class PlayerPage extends StatefulWidget {
 }
 
 class _PlayerPageState extends State<PlayerPage> {
-  //PlayerFR playerFR = PlayerFR();
+  /* AudioPlayer player = AudioPlayer();  //add this
+  AudioCache cache = new AudioCache(); */
+
+  PlayerFR playerFR = PlayerFR();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+ /*  openingActions() async { //add this
+    player = await cache.play('song/teste.mp3'); //add this
+  }  */
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +33,7 @@ class _PlayerPageState extends State<PlayerPage> {
         title: Text("Relax Sound"),
         centerTitle: true,
       ),
-      body: body(),
+      body: Body(),
     );
   }
 }
--- lib/ViewLayer/Widgets/PlayerWidget.dart
+++ lib/ViewLayer/Widgets/PlayerWidget.dart
@@ -2,88 +2,155 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 
-Widget aindanaoseionome(){
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceAround,
-    children: [
-      IconButton(
-        padding: EdgeInsets.all(0),
-        icon:Icon(
-          Icons.skip_previous,
-          color: Colors.pink[200],
-          size: 50.0,
+class PlayerBar extends StatefulWidget {
+  @override
+  _PlayerBarState createState() => _PlayerBarState();
+}
+
+class _PlayerBarState extends State<PlayerBar> {
+  bool _isPlaying = false;
+
+  bool _getIsPlaying(){
+    return _isPlaying;
+  }
+  
+  void setIsPlaying(){
+    setState(() {
+      _isPlaying = !_isPlaying;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        IconButton(
+          padding: EdgeInsets.all(0),
+          icon: Icon(
+            Icons.refresh,
+            color: Colors.pink[200],
+            size: 40.0,
+          ), 
+          onPressed: null
         ),
-        onPressed: () {
-          print("Skip previous");
-        },
-      ),
-      IconButton(
-        padding: EdgeInsets.all(0),
-        icon: (true) 
-          ? Icon(
-              Icons.pause,
-              color: Colors.pink[200],
-              size: 50.0,
-            )
-          : Icon(
-              Icons.play_arrow,
-              color: Colors.pink[200],
-              size: 50.0,
+        IconButton(
+            padding: EdgeInsets.all(0),
+            icon: (_getIsPlaying()) 
+              ? Icon(
+                  Icons.pause,
+                  color: Colors.pink[200],
+                  size: 50.0,
+                )
+              : Icon(
+                  Icons.play_arrow,
+                  color: Colors.pink[200],
+                  size: 50.0,
+              ),
+            onPressed: () {
+              setIsPlaying();
+            },
+        ),
+        IconButton(
+          padding: EdgeInsets.all(0),
+          icon: Icon(
+            Icons.access_time,
+            color: Colors.pink[200],
+            size: 40.0,
+          ), 
+          onPressed: null
+        ),
+      ],
+    );
+  }
+}
+
+class VolumeBar extends StatefulWidget {
+  @override
+  _VolumeBarState createState() => _VolumeBarState();
+}
+
+class _VolumeBarState extends State<VolumeBar> {
+  double _value = 1;
+
+  void setValue(value){
+    setState(() {
+      _value = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Icon(
+          Icons.volume_mute,
+          color: Colors.blue[200],
+          size: 20.0,
+        ),
+        Expanded(
+          child: Slider.adaptive(
+            value: _value,
+            max: 10,
+            onChanged: (value){
+              setValue(value);
+            },
           ),
-        onPressed: () {
-        },
-      ),
-      IconButton(
-        padding: EdgeInsets.all(0),
-        icon: Icon(
-          Icons.skip_next,
-          color: Colors.pink[200],
-          size: 50.0,
         ),
-        onPressed: () {
-          print("skip next");
-        },
-      ),
-    ],
-  );
+        Icon(
+          Icons.volume_up,
+          color: Colors.blue[200],
+          size: 20.0,
+        ),
+      ],
+    );
+  }
 }
 
-Widget content(){
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Container(
-        color: Colors.green[100],
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
-          child: Image.asset("assets/image/mar_bg2.jpg"),
-        ),
-      ),
-      Container(
-        color: Colors.yellow[100],
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-          child: Text("Title"),
-        ),
-      ),
-      Container(
-        color: Colors.blue[200],
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-          child: aindanaoseionome(),
-        ),
-      )
-    ],
-  );
-}
 
-Widget body(){
-  return Container(
-    child: Padding(
-      padding: const EdgeInsets.all(10),
-      child: Center(
-        child: content(),
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.green[100],
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(40, 10, 40, 10),
+                  child: Image.asset("assets/image/mar_bg2.jpg"),
+                ),
+              ),
+              Container(
+                color: Colors.yellow[100],
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: Text("Title"),
+                ),
+              ),
+              Container(
+                color: Colors.blue[200],
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: PlayerBar(),
+                ),
+              ),
+              Container(
+                color: Colors.red[200],
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: VolumeBar(),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
